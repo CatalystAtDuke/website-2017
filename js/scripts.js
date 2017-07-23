9b81c96c1fd67a2acfbd2d048d1af067b934a7b3
Carolyn Sun <carolyn.sun7@gmail.com>
2017-07-22 18:32:54 -0700

    added randomize to flexslider

--- js/scripts.js
+++ js/scripts.js
@@ -132,7 +132,7 @@ $(window).load(function() {
 	$('#teamSlider').flexslider({
 		animation: "slide",
 		directionNav: true,
-        randomizer: true,
+        randomize: true,
 		controlNav: true,
 		touch: true,
 		pauseOnHover: true,
